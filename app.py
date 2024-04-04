1651136812198fe80bee33822551add8c5800012
ombluetuneAI <1571314213@qq.com>
2024-04-04 10:12:46 +0800

    rand_music和rand_radio接口

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import requests
 import time
 import configparser
 import json
+import os
+import csv
+import random
+from datetime import datetime, timedelta
 
 disallow_type = ["Content-Encoding", "Content-Length", "Date", "Server", "Connection", "Transfer-Encoding", "Access-Control-Allow-Origin", 
                  "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Strict-Transport-Security"]
@@ -29,6 +33,19 @@ def config_init():
     
     print(file_server_token, qweather_key)
 
+def csv_read_list(path):
+
+    data = []
+    if not os.path.exists(path):
+        return data
+
+    with open(path, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+
+        for row in reader:
+            data.append(row)
+
+    return data
 
 location = "113.96,22.57"
 def get_weather():
@@ -44,35 +61,111 @@ def get_qweather(query):
 #     print(r.text)
 #     return r.status_code, r.headers, r.content
 
-def get_netease_music(query):
-    r_data = {"code": 1,
-              "data":{
-                  "name": None,
-                  "artistsname": None,
-                  "url": None,
-                  "picurl": None
-              }
-            }
-    retry_cnt = 3
+def netease_get_rand_music():
+    info = {
+        "name": None,
+        "artistsname": None,
+        "url": None,
+        "picurl": None
+    }
+    retry_cnt = 10
     while (retry_cnt):
         print(f"request: {retry_cnt}")
         retry_cnt = retry_cnt - 1
         try:
-            r = requests.get(f"https://api.cenguigui.cn/api/netease/", timeout=5)
-            name = r.json()["data"]["song_name"]
-            artistsname = r.json()["data"]["artist"]
-            url = r.json()["data"]["play_url"]
-            picurl = r.json()["data"]["img"]
-            print(url)
-            if (url[-3:] != "404" and name and artistsname and url and picurl):
-                r_data["data"]["name"] = name
-                r_data["data"]["artistsname"] = artistsname
-                r_data["data"]["url"] = url.replace("https://", "http://")
-                r_data["data"]["picurl"] = picurl.replace("https://", "http://")
-                return r.status_code, r.headers, json.dumps(r_data).encode("utf-8")
+            music =  csv_read_list("netease_music.csv")
+            # 读取随机行
+            rand_row = random.choice(music)
+            name = rand_row[0]
+            artistsname = rand_row[1]
+            url = rand_row[2]
+            picurl = rand_row[3]
+            r = requests.get(url)
+            url = r.url
+            if (url[-3:] != "404"):
+                info["name"] = name
+                info["artistsname"] = artistsname
+                info["url"] = url
+                info["picurl"] = picurl
+                return info
         except:
-            print(f"request err: {retry_cnt}")
-    return None, None, None
+            print("get err")
+    return None
+
+# def get_netease_music(query):
+#     r_data = {"code": 1,
+#               "data":{
+#                   "name": None,
+#                   "artistsname": None,
+#                   "url": None,
+#                   "picurl": None
+#               }
+#             }
+#     retry_cnt = 3
+#     while (retry_cnt):
+#         print(f"request: {retry_cnt}")
+#         retry_cnt = retry_cnt - 1
+#         try:
+#             r = requests.get(f"https://api.cenguigui.cn/api/netease/", timeout=5)
+#             name = r.json()["data"]["song_name"]
+#             artistsname = r.json()["data"]["artist"]
+#             url = r.json()["data"]["play_url"]
+#             picurl = r.json()["data"]["img"]
+#             print(url)
+#             if (url[-3:] != "404" and name and artistsname and url and picurl):
+#                 r_data["data"]["name"] = name
+#                 r_data["data"]["artistsname"] = artistsname
+#                 r_data["data"]["url"] = url.replace("https://", "http://")
+#                 r_data["data"]["picurl"] = picurl.replace("https://", "http://")
+#                 return r.status_code, r.headers, json.dumps(r_data).encode("utf-8")
+#         except:
+#             print(f"request err: {retry_cnt}")
+#     return None, None, None
+
+def get_rand_music(query):
+    music_info = netease_get_rand_music()
+    if (music_info):
+        r_data = {
+            "code": 1,
+            "data": music_info
+        }
+        return json.dumps(r_data).encode("utf-8")
+    return None
+
+def fm_get_cur_id_info(id):
+    music_infos = []
+    r = requests.get(f"https://webapi.qingting.fm/api/pc/radio/{id}")
+    data = r.json()
+    yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y%m%d')
+    picurl = data["album"]["cover"]
+    name = data["album"]["title"]
+    for day in data["pList"]:
+        d_data = data["pList"][day]
+        for i in d_data:
+            artistsname = i["title"]
+            start_time = i["start_time"].replace(":", "")
+            end_time = i["end_time"].replace(":", "")
+            url = f"http://lcache.qtfm.cn/cache/{yesterday}/{id}/{id}_{yesterday}_{start_time}_{end_time}_24_0.aac"
+            music_infos.append({
+                "name": name,
+                "artistsname": artistsname,
+                "url": url,
+                "picurl": picurl
+            })
+        break
+    return music_infos
+
+def get_rand_radio(query):
+    ids = csv_read_list("fm_id.csv")
+    music_infos = fm_get_cur_id_info(random.choice(ids)[0])
+    music_info = random.choice(music_infos)
+    if (music_info):
+        r_data = {
+            "code": 1,
+            "data": music_info
+        }
+        return json.dumps(r_data).encode("utf-8")
+    return None
 
 # 自定义的请求处理程序
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -110,20 +203,41 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
                     self.send_header(type, header[type])
             self.end_headers()
             self.wfile.write(data)
-        elif (path == "/netease_music"):
-            status_code, header, data = get_netease_music(query)
+        # elif (path == "/netease_music"):
+        #     status_code, header, data = get_netease_music(query)
+        #     if (data):
+        #         print(header, len(data))
+        #         self.send_response(status_code)
+        #         self.send_header("Content-Length", len(data))
+        #         for type in header:
+        #             if (type not in disallow_type):
+        #                 self.send_header(type, header[type])
+        #         self.end_headers()
+        #         self.wfile.write(data)
+        #     else:
+        #         self.send_response(404)
+        #         self.end_headers()
+        elif (path == "/rand_music"):
+            data = get_rand_music(query)
             if (data):
-                print(header, len(data))
-                self.send_response(status_code)
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
                 self.send_header("Content-Length", len(data))
-                for type in header:
-                    if (type not in disallow_type):
-                        self.send_header(type, header[type])
                 self.end_headers()
                 self.wfile.write(data)
             else:
                 self.send_response(404)
+        elif (path == "/rand_radio"):
+            data = get_rand_radio(query)
+            if (data):
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.send_header("Content-Length", len(data))
                 self.end_headers()
+                self.wfile.write(data)
+            else:
+                self.send_response(404)
+
 
 config_init()
 server_address = ('', 8888)
